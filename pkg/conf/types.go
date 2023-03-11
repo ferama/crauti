6e8c635c2d122804ec57650b62b91d5ecfc5c165
Marco Ferragina <marco.ferragina@gmail.com>
2023-03-11 20:11:58 +0100

    yaml tags for better config dump

--- pkg/conf/types.go
+++ pkg/conf/types.go
@@ -1,14 +1,20 @@
 package conf
 
 type MountPoint struct {
-	Path     string
-	Upstream string
+	// crauti gateway mount path
+	// like /api/config
+	Path string `yaml:"path"`
+	// full upstream definition
+	// like http://my-service.my-namespace:port
+	Upstream string `yaml:"upstream"`
 }
 
 // config holds all the config values
 type config struct {
-	MountPoints           []MountPoint
-	K8sAutodiscover       bool
-	GatewayListenAddress  string
-	AdminApiListenAddress string
+	MountPoints []MountPoint `yaml:"mountPoints"`
+	// if the service informer is enabled or not
+	K8sAutodiscover bool `yaml:"k8sAutodiscover"`
+
+	GatewayListenAddress  string `yaml:"gatewayListenAddress"`
+	AdminApiListenAddress string `yaml:"adminApiListenAddress"`
 }
