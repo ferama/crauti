c36c318814a9e96c939e42fd1e7d9c46abb957ac
Marco Ferragina <marco.ferragina@gmail.com>
2023-03-11 20:22:59 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+build() {
+    EXT=""
+    [[ $GOOS = "windows" ]] && EXT=".exe"
+    echo "Building ${GOOS} ${GOARCH}"
+    go build \
+        -trimpath \
+        -o ./bin/crauti-${GOOS}-${GOARCH}${EXT} .
+}
+
+### multi arch binary build
+GOOS=linux GOARCH=arm64 build
+GOOS=linux GOARCH=amd64 build
+
+GOOS=darwin GOARCH=arm64 build
+
+GOOS=windows GOARCH=amd64 build
