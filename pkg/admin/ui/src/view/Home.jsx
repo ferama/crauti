cf6296ffd2a5e234b04f3c5903103c4ee1753c6d
Marco Ferragina <marco.ferragina@gmail.com>
2023-03-27 11:16:51 +0200

    display mountpoints in ui

--- pkg/admin/ui/src/view/Home.jsx
+++ pkg/admin/ui/src/view/Home.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import { http } from '../lib/Axios'
+import YAML from 'yaml'
 
 export class Home extends Component {
   state = {
     config: {}
   }
+  intervalHandler = null
 
   async componentDidMount() {
     await this.updateState()
+    this.intervalHandler = setInterval(this.updateState, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalHandler)
   }
 
   updateState = async () => {
@@ -19,34 +26,53 @@ export class Home extends Component {
     } catch {
         return
     }
-    console.log(data)
+    if (data.data === null) return
+    this.setState({
+        "config": data.data
+    })
   }
 
   render() {
+    let rows = (<></>)
+    if (this.state.config.MountPoints !== undefined) {
+      rows = this.state.config.MountPoints.map(mp => (
+        <tr key={mp.Path}>
+          <td>{mp.Path}</td>
+          <td>{mp.Upstream}</td>
+        </tr>
+      ))
+    }
+
+    let middlewares = (<></>)
+    if (this.state.config.Middlewares !== undefined)  {
+      let d = new YAML.Document()
+      d.contents = this.state.config.Middlewares
+      middlewares = d.toString()
+    }
+
     return (
       <Container>
         <Row>
-          <Col><h1>Home</h1></Col>
+          <Col><h3>Middlewares</h3></Col>
+        </Row>
+        <Row>
+          <Col><pre>{middlewares}</pre></Col>
+        </Row>
+        <Row>
+          <Col><h3>MountPoints</h3></Col>
         </Row>
         <Row>
           <Col>
             <Table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Col</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>value1</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>value2</td>
-              </tr>
-            </tbody>
+              <thead>
+                <tr>
+                  <th>MountPoint</th>
+                  <th>Upstream</th>
+                </tr>
+              </thead>
+              <tbody>
+                {rows}
+              </tbody>
             </Table>
           </Col>
         </Row>
