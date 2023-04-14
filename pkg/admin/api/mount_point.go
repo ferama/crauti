7f0bcca0c7ca68b7c106e7abc754d78771fd3135
Marco Ferragina <marco.ferragina@gmail.com>
2023-04-14 08:49:35 +0200

    put method in mount point api

--- pkg/admin/api/mount_point.go
+++ pkg/admin/api/mount_point.go
@@ -2,13 +2,16 @@ package api
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/ferama/crauti/pkg/conf"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
-type mountPointGroup struct{}
+type mountPointGroup struct {
+	mu sync.Mutex
+}
 
 func mountPointRoutes(router *gin.RouterGroup) {
 	r := mountPointGroup{}
@@ -17,7 +20,8 @@ func mountPointRoutes(router *gin.RouterGroup) {
 	router.PUT("", r.put)
 	router.DELETE("", r.delete)
 }
-func (r *mountPointGroup) filter(path, host string) []conf.MountPoint {
+
+func (r *mountPointGroup) search(path, host string) []conf.MountPoint {
 	res := make([]conf.MountPoint, 0)
 	for _, m := range conf.ConfInst.MountPoints {
 		if m.Path == path && m.MatchHost == host {
@@ -28,6 +32,9 @@ func (r *mountPointGroup) filter(path, host string) []conf.MountPoint {
 }
 
 func (r *mountPointGroup) get(c *gin.Context) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	path := c.Query("path")
 	host := c.Query("host")
 
@@ -36,16 +43,75 @@ func (r *mountPointGroup) get(c *gin.Context) {
 		return
 	}
 
-	c.YAML(http.StatusOK, r.filter(path, host))
+	c.YAML(http.StatusOK, r.search(path, host))
 }
 
 func (r *mountPointGroup) post(c *gin.Context) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	var mp conf.MountPoint
+	err := c.BindJSON(&mp)
+	if err != nil {
+		c.YAML(http.StatusInternalServerError, err)
+	}
+	result := r.search(mp.Path, mp.MatchHost)
+
+	if len(result) > 0 {
+		c.YAML(http.StatusBadRequest, "already exists")
+		return
+	}
+
+	mountPoints := make([]conf.MountPoint, 0)
+	mountPoints = append(mountPoints, conf.ConfInst.MountPoints...)
+	mountPoints = append(mountPoints, mp)
+
+	viper.Set("MountPoints", mountPoints)
+	err = viper.WriteConfig()
+	if err != nil {
+		c.YAML(http.StatusInternalServerError, "can't update config")
+	}
+
+	c.YAML(http.StatusOK, mp)
 }
 
 func (r *mountPointGroup) put(c *gin.Context) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	var mp conf.MountPoint
+	err := c.BindJSON(&mp)
+	if err != nil {
+		c.YAML(http.StatusInternalServerError, err)
+	}
+	result := r.search(mp.Path, mp.MatchHost)
+
+	if len(result) == 0 {
+		c.YAML(http.StatusBadRequest, "mount point doesn't exists")
+		return
+	}
+
+	mountPoints := make([]conf.MountPoint, 0)
+	for _, m := range conf.ConfInst.MountPoints {
+		if m.Path != result[0].Path || m.MatchHost != result[0].MatchHost {
+			mountPoints = append(mountPoints, m)
+		}
+	}
+	mountPoints = append(mountPoints, mp)
+
+	viper.Set("MountPoints", mountPoints)
+	err = viper.WriteConfig()
+	if err != nil {
+		c.YAML(http.StatusInternalServerError, "can't update config")
+	}
+
+	c.YAML(http.StatusOK, mp)
 }
 
 func (r *mountPointGroup) delete(c *gin.Context) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	path := c.Query("path")
 	host := c.Query("host")
 	if path != "" {
@@ -56,6 +122,9 @@ func (r *mountPointGroup) delete(c *gin.Context) {
 			}
 		}
 		viper.Set("MountPoints", res)
-		viper.WriteConfig()
+		err := viper.WriteConfig()
+		if err != nil {
+			c.YAML(http.StatusInternalServerError, "can't update config")
+		}
 	}
 }
