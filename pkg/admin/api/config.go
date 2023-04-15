4998977a7138d7a5b65e25aa9d15a66a2b5f8e66
Marco Ferragina <marco.ferragina@gmail.com>
2023-04-15 09:33:20 +0200

    added writeable endpoint

--- pkg/admin/api/config.go
+++ pkg/admin/api/config.go
@@ -2,18 +2,23 @@ package api
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/ferama/crauti/pkg/conf"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
-type configGroup struct{}
+type configGroup struct {
+	mu sync.Mutex
+}
 
 // Routes setup the root api routes
 func configRoutes(router *gin.RouterGroup) {
 	r := &configGroup{}
 
 	router.GET("", r.config)
+	router.GET("/writeable", r.writeable)
 }
 
 func (r *configGroup) config(c *gin.Context) {
@@ -21,4 +26,21 @@ func (r *configGroup) config(c *gin.Context) {
 		Data:    conf.ConfInst,
 		Offered: supportedFormats,
 	})
+}
+
+func (r *configGroup) writeable(c *gin.Context) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	err := viper.WriteConfig()
+
+	var res struct {
+		Writeable bool `json:"writeable"`
+	}
+	res.Writeable = true
+	if err != nil {
+		res.Writeable = false
+	}
+
+	c.JSON(http.StatusOK, res)
 }
